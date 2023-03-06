42712e9ce1c38546468eb4555a7b6f07747cc007
Bogdan <bogdanlazarpopescu@gmail.com>
2023-03-06 08:47:24 +0000

    set up project

---
+++ bin/cdk-test.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkTestStack } from '../lib/cdk-test-stack';
+
+const app = new cdk.App();
+new CdkTestStack(app, 'CdkTestStack', {
+
+  env: { account: '452280938609', region: 'eu-west-1' },
+
+});
