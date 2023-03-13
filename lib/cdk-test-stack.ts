a8397df1873cff3d10e8b6ff268440f49fca4dcf
Bogdan <bogdanlazarpopescu@gmail.com>
2023-03-13 09:53:27 +0000

    test

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -97,6 +97,15 @@ export class CdkTestStack extends cdk.Stack {
           referenceName: ['main'],
           event: ['referenceUpdated'],
         }
+        // detail: {
+        //   event: ["referenceUpdated"],
+        //   referenceType: ["branch"],
+        //   referenceName: ["main"],
+        //   commitId: [{
+        //     prefix: "github",
+        //     exists: true
+        //   }]
+        // }
       }
     })
   }
