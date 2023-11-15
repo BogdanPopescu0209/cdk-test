778b8318d36e8a45b918198f54b695c39eb16e6c
Bogdan <bogdanlazarpopescu@gmail.com>
2023-11-15 21:42:02 +0200

    test: pipeline

--- bin/cdk-test.ts
+++ bin/cdk-test.ts
@@ -2,8 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkTestStack } from '../lib/cdk-test-stack-pipeline';
 
 const app = new cdk.App();
+
 new CdkTestStack(app, 'CdkTestStack', {
 
   env: { account: '452280938609', region: 'eu-west-1' },
+
+});
+
+new CdkTestStack(app, 'CdkTestStack', {
+
+  env: { account: '452280938609', region: 'us-west-2' },
 
 });
