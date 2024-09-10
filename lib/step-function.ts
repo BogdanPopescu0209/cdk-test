e96e9ba648049bae1f5ff855ddf9aff53f23a719
Bogdan <bogdanlazarpopescu@gmail.com>
2024-09-10 15:17:26 +0100

    test

--- lib/step-function.ts
+++ lib/step-function.ts
@@ -6,10 +6,10 @@ import * as cdk from 'aws-cdk-lib';
 
 export function stepFunctionSandbox(scope: Construct) {
 
-    const testStateParallel = new stepfunctions.Parallel(scope, 'TestStateParallel', {});
+    // const testStateParallel = new stepfunctions.Parallel(scope, 'TestStateParallel', {});
 
-    const stateOne = new stepfunctions.Pass(scope, 'StateOne', {})
-    const stateTwo = new stepfunctions.Pass(scope, 'StateTwo', {})
+    // const stateOne = new stepfunctions.Pass(scope, 'StateOne', {})
+    // const stateTwo = new stepfunctions.Pass(scope, 'StateTwo', {})
 
     const sandboxLogGroup = new logs.LogGroup(scope, 'Step Functions Log Group', {
         logGroupName: `sandbox-pipeline-test`,
